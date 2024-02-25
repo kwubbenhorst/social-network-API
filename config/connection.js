57b11f42c0a80ee0e76a729fbe105e8c0a2375dd
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2024-02-24 21:18:01 -0500

    changes to config and package.json to reflect repo name change

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require('mongoose');
 
 // Leaving it flexible in case I want to create a front end and deploy later. Meanwhile the || option will connect the server locally 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social_network_db';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/thought_share_db';
 
 connect(connectionString);
 
